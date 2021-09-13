f2e551e2144267589552d5d203d885f3a72aee42
Pavel-Grabovski <Araa9977@mail.ru>
2021-09-13 16:31:07 +0700

    one commit

---
+++ MyIndoorClimate/Add.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Reflection;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Media;
+
+
+
+namespace MyIndoorClimate.Acoustics
+{
+    public class Add : IExternalApplication
+    {
+        string RibbonTab = "MyIndoorClimate";
+        string RibbonPanel = "Расчет звукоизоляции";
+        public Result OnStartup(UIControlledApplication application)
+        {
+            try
+            {
+                application.CreateRibbonTab(RibbonTab);
+            }
+            catch (Exception) { }//tab adready exists*/
+            // get or create the panet
+            RibbonPanel ribbonPanel = null;
+            foreach(RibbonPanel pnl in application.GetRibbonPanels(RibbonTab))
+            {
+                if (pnl.Name == RibbonPanel)
+                {
+                    ribbonPanel = pnl;
+                    break;
+                }
+            }
+            if (ribbonPanel == null)
+            {
+                ribbonPanel = application.CreateRibbonPanel(RibbonTab, RibbonPanel);
+            }
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            PushButtonData buttonData = new PushButtonData("cmdAcoustics", "Расчет \n звукоизоляции",
+                thisAssemblyPath, "MyIndoorClimate.Acoustics.Command");
+
+            PushButton pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
+
+            // При желании кнопке могут быть присвоены другие свойства.
+            // a) Подсказка
+            pushButton.ToolTip = "Расчет звукоизоляции стен и перекрытий";
+            
+            Image ImageRebarSquareColumns = global::MyIndoorClimate.Properties.Resources.pngwing;
+            ImageSource _imageSoursRebarSquareColumns = GetImageSourse(ImageRebarSquareColumns);
+            pushButton.LargeImage = _imageSoursRebarSquareColumns;
+
+            
+
+
+            return Result.Succeeded;
+        }
+        private ImageSource GetImageSourse(Image img)
+        {
+            BitmapImage bmp = new BitmapImage();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, ImageFormat.Png);
+                ms.Position = 0;
+
+                bmp.BeginInit();
+
+                bmp.CacheOption = BitmapCacheOption.OnLoad;
+                bmp.UriSource = null;
+                bmp.StreamSource = ms;
+
+                bmp.EndInit();
+            }
+            return bmp;
+        }
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+    }
+}
---
+++ MyIndoorClimate/Command.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyIndoorClimate.Acoustics
+{
+    // Транзакция, обязательно для запуска команды!
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            MessageBox.Show("Привет");
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+    }
+}
